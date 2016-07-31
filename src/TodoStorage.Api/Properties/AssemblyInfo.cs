d1c3ecc8d9915c44021ad0009ed06b1225a24b2d
Simon Wendel <mail@simonwendel.se>
2016-07-31 16:39:28 +0200

    feat: Add Web API v2 project

---
+++ src/TodoStorage.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Todo App Storage API")]
+[assembly: AssemblyProduct("TodoStorage.Api")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("05f55ee7-f003-43de-b842-4395bd8a25e2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
