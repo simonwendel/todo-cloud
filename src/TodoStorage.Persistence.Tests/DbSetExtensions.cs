05b52486e646db225948486c07107f12dda6145e
Simon Wendel <mail@simonwendel.se>
2016-09-01 21:41:54 +0200

    test: Add extension method to clear a DbSet

---
+++ src/TodoStorage.Persistence.Tests/DbSetExtensions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Persistence.Tests
+{
+    using System.Data.Entity;
+    using Domain;
+
+    internal static class DbSetExtensions
+    {
+        public static void Clear<T>(this DbSet<T> set) where T : class
+        {
+            Guard.NullParameter(set, nameof(set));
+            set.RemoveRange(set);
+        }
+    }
+}
---
+++ src/TodoStorage.Persistence.Tests/DbSetExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Persistence.Tests
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DbSetExtensionsTests
+    {
+        [Test]
+        public void Clear_GivenNullDbSet_ThrowsException()
+        {
+            DbSet<object> nullSet = null;
+
+            Assert.That(() => nullSet.Clear(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Clear_GivenDbSet_CallsRemoveRangeWithSelf()
+        {
+            var mockSet = new Mock<DbSet<object>>();
+            IEnumerable<object> parameter = null;
+
+            mockSet
+                .Setup(db => db.RemoveRange(It.IsAny<IEnumerable<object>>()))
+                .Callback((IEnumerable<object> param) =>
+                {
+                    parameter = param;
+                });
+
+            mockSet.Object.Clear();
+
+            Assert.That(parameter, Is.SameAs(mockSet.Object));
+            mockSet.VerifyAll();
+        }
+    }
+}
