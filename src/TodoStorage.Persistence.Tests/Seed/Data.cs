e80dbb764ce50c9fdd3e7d525ece2a1872a4d023
Simon Wendel <mail@simonwendel.se>
2016-09-02 13:51:30 +0200

    test: Add some data to use when seeding test db

---
+++ src/TodoStorage.Persistence.Tests/Seed/Data.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Persistence.Tests.Seed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Domain;
+
+    internal static class Data
+    {
+        public static readonly CollectionKey TestCollectionKey = new CollectionKey(Guid.NewGuid());
+
+        public static readonly IList<TodoItem> PersistedItems = new List<TodoItem>
+            {
+                new TodoItem { Title = "Should be found (1)", Description = "Some kind of description 1.", ColorName = "Röd", ColorValue = "Red", Created = DateTime.Now, NextOccurrence = null, Recurring = 0, StorageKey = TestCollectionKey.Identifier },
+                new TodoItem { Title = "Should be found (2)", Description = "Some kind of description 2.", ColorName = "Orange", ColorValue = "Orange", Created = DateTime.Now, NextOccurrence = null, Recurring = 0, StorageKey = TestCollectionKey.Identifier },
+                new TodoItem { Title = "Should be found (3)", Description = "Some kind of description 3.", ColorName = "Orange", ColorValue = "Orange", Created = DateTime.Now, NextOccurrence = null, Recurring = 0, StorageKey = TestCollectionKey.Identifier },
+                new TodoItem { Title = "Should not be found (1)", Description = "Some kind of description 4.", ColorName = "Grön", ColorValue = "Green", Created = DateTime.Now, NextOccurrence = null, Recurring = 0, StorageKey = Guid.NewGuid() },
+                new TodoItem { Title = "Should not be found (2)", Description = "Some kind of description 5.", ColorName = "Röd", ColorValue = "Red", Created = DateTime.Now, NextOccurrence = null, Recurring = 0, StorageKey = Guid.NewGuid() }
+            };
+
+        public static IList<Todo> DomainObjects => 
+            PersistedItems.Select(item =>
+                new Todo
+                {
+                    Id = item.Id,
+                    Title = item.Title,
+                    Description = item.Description,
+                    Color = new Color(item.ColorName, item.ColorValue),
+                    Created = item.Created,
+                    Recurring = item.Recurring,
+                    NextOccurrence = item.NextOccurrence
+                })
+            .ToList();
+    }
+}
