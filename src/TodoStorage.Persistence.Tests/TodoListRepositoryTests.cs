16218330580a595735a74be8018ea3b97fed6da4
Simon Wendel <mail@simonwendel.se>
2016-09-09 17:35:06 +0200

    feat: Implement ITodoListRepository in persistence project

---
+++ src/TodoStorage.Persistence.Tests/TodoListRepositoryTests.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Persistence.Tests
+{
+    using Domain;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+
+    [TestFixture]
+    internal class TodoListRepositoryTests
+    {
+        private SqlServerConnectionFactory connectionFactory;
+
+        private TodoListRepository sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            var resolver = new ConnectionStringResolver("TodoStorage");
+
+            connectionFactory = new SqlServerConnectionFactory(resolver);
+            sut = new TodoListRepository(connectionFactory);
+        }
+
+        [Test]
+        public void Ctor_GivenNullDbConnectionFactory_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new TodoListRepository(null);
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void GetList_GivenNullCollectionKey_ThrowsException()
+        {
+            TestDelegate getListCall =
+                 () => sut.GetList(null);
+
+            Assert.That(getListCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void GetList_GivenNonPersistedCollectionKey_ReturnsNull()
+        {
+            var fixture = new Fixture();
+            var key = fixture.Create<CollectionKey>();
+
+            var actual = sut.GetList(key);
+
+            Assert.That(actual, Is.Null);
+        }
+
+        [Test]
+        public void GetList_GivenCollectionKey_ReturnsTodoList()
+        {
+            var key = Seed.Data.TestCollectionKey;
+            var expected = Seed.Data.TestList;
+
+            var actual = sut.GetList(key);
+
+            Assert.That(actual.Equals(expected));
+        }
+    }
+}
---
+++ src/TodoStorage.Persistence/TodoListRepository.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Persistence
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using Dapper;
+    using Domain;
+
+    internal class TodoListRepository : ITodoListRepository
+    {
+        private const string TodoSelectionSql = @"
+SELECT
+    [Id],
+    [StorageKey],
+    [Title],
+    [Description],
+    [Created],
+    [Recurring],
+    [NextOccurrence],
+    [ColorName],
+    [ColorValue]
+FROM
+    [TodoItem]
+WHERE
+    [StorageKey] = @StorageKey
+ORDER BY
+    [Id]";
+
+        private readonly IDbConnectionFactory connectionFactory;
+
+        public TodoListRepository(IDbConnectionFactory connectionFactory)
+        {
+            Guard.NullParameter(connectionFactory, nameof(connectionFactory));
+
+            this.connectionFactory = connectionFactory;
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0", Justification = "I am.")]
+        public TodoList GetList(CollectionKey collectionKey)
+        {
+            Guard.NullParameter(collectionKey, nameof(collectionKey));
+
+            using (var connection = connectionFactory.GetConnection())
+            {
+                var whereConstraint = new { StorageKey = collectionKey.Identifier };
+                var todos = connection.Query<Todo, Color, Todo>(
+                    TodoSelectionSql,
+                    MapTodoProperties,
+                    whereConstraint,
+                    splitOn: "ColorName");
+
+                if (todos.Count() == 0)
+                {
+                    return null;
+                }
+
+                return new TodoList(collectionKey, todos);
+            }
+        }
+
+        private static Todo MapTodoProperties(Todo todo, Color color)
+        {
+            todo.Color = color;
+            return todo;
+        }
+    }
+}
