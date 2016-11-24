eae5c74ec169c5f0fe98728a8e9b77cdfcb230b4
Simon Wendel <mail@simonwendel.se>
2016-11-24 18:34:36 +0100

    feat: Add IMessageHasher with impl

---
+++ src/TodoStorage.Security.Tests/MessageHasherTests.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Security.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+
+    [TestFixture]
+    internal class MessageHasherTests : IDisposable
+    {
+        private MessageHasher sut;
+
+        private HMACSHA256 algorithm;
+
+        private byte[] key;
+
+        private string message;
+
+        [SetUp]
+        public void Setup()
+        {
+            var fixture = new Fixture();
+
+            message = fixture.Create<string>();
+            key = fixture.CreateMany<byte>(32).ToArray();
+
+            algorithm = new HMACSHA256(key);
+
+            sut = new MessageHasher(algorithm);
+        }
+
+        [Test]
+        public void Ctor_GivenNullAlgorithm_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new MessageHasher(null);
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void HashMessage_GivenNullMessage_ThrowsException()
+        {
+            TestDelegate hashMessageCall =
+                () => sut.HashMessage(null);
+
+            Assert.That(hashMessageCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void HashMessage_GivenMessageAndKey_ComnputesHash()
+        {
+            var messageBytes = Encoding.Unicode.GetBytes(message);
+            var expectedHash = algorithm.ComputeHash(messageBytes);
+
+            var hash = sut.HashMessage(message);
+
+            Assert.That(hash, Is.EquivalentTo(expectedHash));
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (algorithm != null)
+                {
+                    algorithm.Dispose();
+                    algorithm = null;
+                }
+            }
+        }
+    }
+}
---
+++ src/TodoStorage.Security/MessageHasher.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Security
+{
+    using System.Security.Cryptography;
+    using System.Text;
+    using SimonWendel.GuardStatements;
+
+    internal class MessageHasher : IMessageHasher
+    {
+        private readonly HashAlgorithm algorithm;
+
+        public MessageHasher(HashAlgorithm algorithm)
+        {
+            Guard.EnsureNotNull(algorithm, nameof(algorithm));
+
+            this.algorithm = algorithm;
+        }
+
+        public byte[] HashMessage(string message)
+        {
+            Guard.EnsureNotNull(message);
+
+            var messageBytes = Encoding.Unicode.GetBytes(message);
+            return algorithm.ComputeHash(messageBytes);
+        }
+    }
+}
