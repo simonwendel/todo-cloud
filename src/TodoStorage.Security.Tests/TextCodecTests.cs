9ae14cb5b4c906248335412f7cac10df940c3547
Simon Wendel <mail@simonwendel.se>
2016-12-04 22:43:53 +0100

    feat: Add ITextCodec and impl to Security domain

---
+++ src/TodoStorage.Security.Tests/TextCodecTests.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Security.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class TextCodecTests
+    {
+        private TextCodec sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new TextCodec();
+        }
+
+        [Test]
+        public void Decode_GivenNullValue_ThrowsException()
+        {
+            TestDelegate decodeCall =
+                () => sut.Decode(null);
+
+            Assert.That(decodeCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Decode_GivenValue_Base64DecodesTheValue()
+        {
+            var decodedValue = sut.Decode("VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZyEhMQ==");
+
+            Assert.That(decodedValue, Is.EqualTo("This is an encoded string!!1"));
+        }
+
+        [Test]
+        public void Encode_GivenNullValue_ThrowsException()
+        {
+            TestDelegate encodeCall =
+                () => sut.Encode(null);
+
+            Assert.That(encodeCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Encode_GivenValue_Base64EncodesTheValue()
+        {
+            var encodedValue = sut.Encode("This is an encoded string!!1");
+
+            Assert.That(encodedValue, Is.EqualTo("VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZyEhMQ=="));
+        }
+    }
+}
---
+++ src/TodoStorage.Security/TextCodec.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Security
+{
+    using System;
+    using System.Text;
+    using SimonWendel.GuardStatements;
+
+    internal class TextCodec : ITextCodec
+    {
+        public string Decode(string value)
+        {
+            Guard.EnsureNotNull(value, nameof(value));
+
+            var encodedValue = Convert.FromBase64String(value);
+            return Encoding.UTF8.GetString(encodedValue);
+        }
+
+        public string Encode(string value)
+        {
+            Guard.EnsureNotNull(value, nameof(value));
+
+            var valueBytes = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(valueBytes);
+        }
+    }
+}
