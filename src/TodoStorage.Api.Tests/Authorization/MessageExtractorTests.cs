b12b0e081f236ec8657005474fa533812e237f44
Simon Wendel <mail@simonwendel.se>
2016-12-23 22:55:04 +0100

    feat: Add implementation of IMessageExtractor

---
+++ src/TodoStorage.Api.Tests/Authorization/MessageExtractorTests.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Tests.Authorization
+{
+    using System;
+    using System.Net.Http.Headers;
+    using Moq;
+    using NUnit.Framework;
+    using TodoStorage.Api.Authorization;
+    using TodoStorage.Api.Tests.Utilities;
+    using TodoStorage.Security;
+
+    [TestFixture]
+    internal class MessageExtractorTests
+    {
+        private MessageExtractor sut;
+
+        private Mock<IMessageFactory> messageFactory;
+
+        private IMessage message;
+
+        [SetUp]
+        public void Setup()
+        {
+            message = Mock.Of<IMessage>();
+
+            messageFactory = new Mock<IMessageFactory>();
+            messageFactory
+                .Setup(f => f.Build(
+                    It.IsAny<Guid>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Uri>(),
+                    It.IsAny<ulong>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                .Returns(message);
+
+            sut = new MessageExtractor(messageFactory.Object);
+        }
+
+        [Test]
+        public void Ctor_GivenNullMessageFactory_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new MessageExtractor(null);
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void ExtractMessage_GivenNullActionContext_ThrowsException()
+        {
+            TestDelegate extractCall =
+                () => sut.ExtractMessage(null);
+
+            Assert.That(extractCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void ExtractMessage_GivenWrongAuthScheme_ThrowsException()
+        {
+            var context = new FakeHttpActionContext();
+            context.Request.Headers.Authorization = new AuthenticationHeaderValue("nope");
+
+            TestDelegate extractCall =
+                () => sut.ExtractMessage(context);
+
+            Assert.That(extractCall, Throws.ArgumentException);
+        }
+
+        [Test]
+        public void ExtractMessage_GivenActionContext_ExtractsMessage()
+        {
+            var extractedMessage = sut.ExtractMessage(new FakeHttpActionContext());
+
+            Assert.That(extractedMessage, Is.SameAs(message));
+            messageFactory.Verify(
+                f => f.Build(
+                    It.Is<Guid>(g => g == FakeHttpActionContext.FakeAppId),
+                    It.Is<string>(s => s.Equals(FakeHttpActionContext.FakeMethod.Method)),
+                    It.Is<Uri>(u => u == FakeHttpActionContext.FakeUri),
+                    It.Is<ulong>(t => t == FakeHttpActionContext.FakeTimestamp),
+                    It.Is<string>(n => n.Equals(FakeHttpActionContext.FakeNonce)),
+                    It.Is<string>(b => b.Equals(FakeHttpActionContext.FakeContent)),
+                    It.Is<string>(s => s.Equals(FakeHttpActionContext.FakeSignature))),
+                Times.Once);
+        }
+    }
+}
---
+++ src/TodoStorage.Api/Authorization/MessageExtractor.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Authorization
+{
+    using System;
+    using System.Globalization;
+    using System.Web.Http.Controllers;
+    using SimonWendel.GuardStatements;
+    using TodoStorage.Security;
+
+    internal class MessageExtractor : IMessageExtractor
+    {
+        private readonly IMessageFactory messageFactory;
+
+        public MessageExtractor(IMessageFactory messageFactory)
+        {
+            Guard.EnsureNotNull(messageFactory, nameof(messageFactory));
+
+            this.messageFactory = messageFactory;
+        }
+
+        public IMessage ExtractMessage(HttpActionContext context)
+        {
+            Guard.EnsureNotNull(context, nameof(context));
+
+            var authHeader = context.Request.Headers.Authorization;
+            var authScheme = authHeader.Scheme;
+
+            Guard.EnsureThat(authScheme.Equals("xhmac"), nameof(context));
+
+            var authParameter = context.Request.Headers.Authorization.Parameter;
+            var fields = authParameter.Split(':');
+
+            var appId = new Guid(fields[0]);
+            var signature = fields[1];
+            var nonce = fields[2];
+            var timestamp = Convert.ToUInt64(fields[3], CultureInfo.InvariantCulture);
+
+            var method = context.Request.Method.Method;
+            var uri = context.Request.RequestUri;
+
+            // no point in turning the whole method into async because of this
+            // we're not waiting on anything else
+            string content;
+            using (var contentTask = context.Request.Content.ReadAsStringAsync())
+            {
+                contentTask.Wait();
+                content = contentTask.Result;
+            }
+
+            return messageFactory.Build(
+                appId, method, uri, timestamp, nonce, content, signature);
+        }
+    }
+}
