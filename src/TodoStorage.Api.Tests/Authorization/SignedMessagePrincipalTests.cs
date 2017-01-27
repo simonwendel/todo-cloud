499209c1a5138b997ab297a0c3349b3689747fbe
Simon Wendel <mail@simonwendel.se>
2017-01-27 15:19:38 +0100

    feat: Add SignedMessagePrincipal to API

---
+++ src/TodoStorage.Api.Tests/Authorization/SignedMessagePrincipalTests.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Tests.Authorization
+{
+    using System.Linq;
+    using Moq;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+    using TodoStorage.Api.Authorization;
+    using TodoStorage.Security;
+
+    [TestFixture]
+    internal class SignedMessagePrincipalTests
+    {
+        private IMessage message;
+
+        private SignedMessagePrincipal sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            message = Mock.Of<IMessage>();
+            sut = new SignedMessagePrincipal(message);
+        }
+
+        [Test]
+        public void Ctor_GivenNullMessage_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new SignedMessagePrincipal(null);
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Ctor_GivenMessage_SetsProperty()
+        {
+            Assert.That(sut.Message, Is.SameAs(message));
+        }
+
+        [Test]
+        public void Identity_IsNull()
+        {
+            Assert.That(sut.Identity, Is.Null);
+        }
+
+        [Test]
+        public void IsInRole_GivenRole_ReturnsFalse()
+        {
+            var fixture = new Fixture();
+            var roles = fixture.CreateMany<string>(100);
+
+            // maybe a weak test, but it does test something. testing that the 
+            // method is always false regardless of input is an extensive problem.
+            Assert.That(roles.All(r => sut.IsInRole(r)), Is.False);
+        }
+    }
+}
---
+++ src/TodoStorage.Api/Authorization/SignedMessagePrincipal.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Authorization
+{
+    using System.Security.Principal;
+    using SimonWendel.GuardStatements;
+    using TodoStorage.Security;
+
+    public class SignedMessagePrincipal : IPrincipal
+    {
+        private IMessage message;
+
+        public SignedMessagePrincipal(IMessage message)
+        {
+            Guard.EnsureNotNull(message);
+
+            this.message = message;
+        }
+
+        public IMessage Message => message;
+
+        public IIdentity Identity => null;
+
+        public bool IsInRole(string role) => false;
+    }
+}
