02375ac4d047d4975635a54955a1f109f30f2dab
Simon Wendel <mail@simonwendel.se>
2017-01-30 19:46:24 +0100

    feat: IHttpActionResult failing request on auth error

---
+++ src/TodoStorage.Api.Tests/Authorization/AuthenticationFailureResultTests.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Tests.Authorization
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using TodoStorage.Api.Authorization;
+
+    [TestFixture]
+    internal class AuthenticationFailureResultTests
+    {
+        [Test]
+        public void Ctor_GivenNullFailureReason_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new AuthenticationFailureResult(null, new HttpRequestMessage());
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Ctor_GivenNullFailingReques_ThrowsException()
+        {
+            TestDelegate constructorCall =
+                () => new AuthenticationFailureResult("It just did!", null);
+
+            Assert.That(constructorCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_ReturnsUnathorizedResponse()
+        {
+            var request = new HttpRequestMessage();
+            var sut = new AuthenticationFailureResult("It just did!", request);
+
+            using (var response = await sut.ExecuteAsync(new CancellationToken()))
+            {
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+                Assert.That(response.ReasonPhrase, Is.EqualTo("It just did!"));
+                Assert.That(response.RequestMessage, Is.SameAs(request));
+            } 
+        }
+    }
+}
---
+++ src/TodoStorage.Api/Authorization/AuthenticationFailureResult.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Authorization
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using SimonWendel.GuardStatements;
+
+    public class AuthenticationFailureResult : IHttpActionResult
+    {
+        private string failureReason;
+
+        private HttpRequestMessage failingRequest;
+
+        public AuthenticationFailureResult(string failureReason, HttpRequestMessage failingRequest)
+        {
+            Guard.EnsureNotNull(failureReason, nameof(failureReason));
+            Guard.EnsureNotNull(failingRequest, nameof(failingRequest));
+
+            this.failureReason = failureReason;
+            this.failingRequest = failingRequest;
+        }
+
+        [SuppressMessage(
+            "Microsoft.Reliability", 
+            "CA2000:Dispose objects before losing scope", 
+            Justification = "The response is registered for dispose at end of request. I count on Web API to do the right thing here.")]
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                RequestMessage = failingRequest,
+                ReasonPhrase = failureReason
+            };
+
+            // I love this extension method! Without it, this is tricky...
+            failingRequest.RegisterForDispose(response);
+
+            return Task.FromResult(response);
+        }
+    }
+}
