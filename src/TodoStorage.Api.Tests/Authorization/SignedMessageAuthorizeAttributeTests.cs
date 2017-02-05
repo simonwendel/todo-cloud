7d07db9f22dd3df740a8802b4ad089c0461bc8bc
Simon Wendel <mail@simonwendel.se>
2017-02-05 21:44:35 +0100

    feat: Add SignedMessageAuthorizeAttribute

---
+++ src/TodoStorage.Api.Tests/Authorization/SignedMessageAuthorizeAttributeTests.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Tests.Authorization
+{
+    using System.Net;
+    using System.Security.Claims;
+    using Moq;
+    using NUnit.Framework;
+    using TodoStorage.Api.Authorization;
+    using TodoStorage.Api.Tests.Utilities;
+    using TodoStorage.Security;
+
+    [TestFixture]
+    internal class SignedMessageAuthorizeAttributeTests
+    {
+        private FakeHttpActionContext context;
+
+        private SignedMessageAuthorizeAttribute sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            context = new FakeHttpActionContext();
+
+            sut = new SignedMessageAuthorizeAttribute();
+        }
+
+        [Test]
+        public void OnAuthorization_GivenNullPrincipal_SetsNotAuthorized()
+        {
+            context.RequestContext.Principal = null;
+
+            sut.OnAuthorization(context);
+
+            Assert.That(context.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void OnAuthorization_GivenOtherPrincipal_SetsNotAuthorized()
+        {
+            context.RequestContext.Principal = new ClaimsPrincipal();
+
+            sut.OnAuthorization(context);
+
+            Assert.That(context.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void OnAuthorization_GivenSignedMessagePrincipal_SetsAuthorized()
+        {
+            context.RequestContext.Principal = new SignedMessagePrincipal(Mock.Of<IMessage>());
+
+            sut.OnAuthorization(context);
+
+            Assert.That(context.Response.StatusCode, Is.Not.EqualTo(HttpStatusCode.Unauthorized));
+        }
+    }
+}
---
+++ src/TodoStorage.Api/Authorization/SignedMessageAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Authorization
+{
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+    using SimonWendel.GuardStatements;
+
+    internal sealed class SignedMessageAuthorizeAttribute : AuthorizeAttribute
+    {
+        /// <summary>
+        /// Will authorize whenever a signed message has been verified and 
+        /// made available to the request. Contents of message is not examined.
+        /// </summary>
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            Guard.EnsureNotNull(actionContext, nameof(actionContext));
+
+            return actionContext.RequestContext.Principal is SignedMessagePrincipal;
+        }
+    }
+}
