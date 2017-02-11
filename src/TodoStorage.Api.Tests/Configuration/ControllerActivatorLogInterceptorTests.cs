c3611e6de7cd51122725ff2d56c5656a7382f84e
Simon Wendel <mail@simonwendel.se>
2017-02-11 23:14:23 +0100

    feat: Add interceptor logging controller construction

---
+++ src/TodoStorage.Api.Tests/Configuration/ControllerActivatorLogInterceptorTests.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Tests.Configuration
+{
+    using System;
+    using Common.Logging;
+    using Moq;
+    using Ninject.Extensions.Interception;
+    using NUnit.Framework;
+    using TodoStorage.Api.Configuration;
+
+    [TestFixture]
+    internal class ControllerActivatorLogInterceptorTests
+    {
+        private ControllerActivatorLogInterceptor sut;
+
+        private Mock<ILog> logger;
+
+        private Mock<IInvocation> invocation;
+
+        [SetUp]
+        public void Setup()
+        {
+            invocation = new Mock<IInvocation>();
+
+            logger = CreateLogger();
+            logger.Setup(
+                l => l.Fatal(It.IsAny<string>(), It.IsAny<Exception>()));
+
+            sut = new ControllerActivatorLogInterceptor();
+        }
+
+        [Test]
+        public void Intercept_GivenNullInvocation_ThrowsException()
+        {
+            TestDelegate interceptCall =
+                () => sut.Intercept(null);
+
+            Assert.That(interceptCall, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Intercept_WhenInvocationThrowsException_LogsExceptionAndRethrows()
+        {
+            var exception = new Exception();
+            invocation
+                .Setup(i => i.Proceed())
+                .Throws(exception);
+
+            TestDelegate interceptCall =
+                () => sut.Intercept(invocation.Object);
+
+            Assert.That(interceptCall, Throws.Exception);
+            logger.Verify(
+                l => l.Fatal(
+                    It.Is<string>(s => s.Equals("Exception caught in Controller Activator")),
+                    It.Is<Exception>(e => e == exception)),
+                Times.Once);
+        }
+
+        [Test]
+        public void Intercept_WhenInvocationSucceeds_DoesNotLogException()
+        {
+            var exception = new Exception();
+            invocation
+                .Setup(i => i.Proceed());
+
+            sut.Intercept(invocation.Object);
+
+            logger.Verify(
+                l => l.Fatal(
+                    It.IsAny<string>(),
+                    It.IsAny<Exception>()),
+                Times.Never);
+        }
+
+        private Mock<ILog> CreateLogger()
+        {
+            var logger = new Mock<ILog>();
+
+            var exception = new Exception();
+
+            var loggerAdapter = new Mock<ILoggerFactoryAdapter>();
+            loggerAdapter
+                .Setup(l => l.GetLogger(typeof(ControllerActivatorLogInterceptor)))
+                .Returns(logger.Object);
+
+            LogManager.Adapter = loggerAdapter.Object;
+
+            return logger;
+        }
+    }
+}
---
+++ src/TodoStorage.Api/Configuration/ControllerActivatorLogInterceptor.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoStorage.Api.Configuration
+{
+    using System;
+    using Common.Logging;
+    using Ninject.Extensions.Interception;
+    using SimonWendel.GuardStatements;
+
+    public class ControllerActivatorLogInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Guard.EnsureNotNull(invocation, nameof(invocation));
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger<ControllerActivatorLogInterceptor>()
+                    .Fatal("Exception caught in Controller Activator", ex);
+
+                throw ex;
+            }
+        }
+    }
+}
