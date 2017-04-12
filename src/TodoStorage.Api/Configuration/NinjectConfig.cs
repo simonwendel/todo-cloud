858866305884c786fec8094c380b02d7e246e0f0
Simon Wendel <mail@simonwendel.se>
2017-04-12 16:28:02 +0200

    fix: Update copyright header dates

--- src/TodoStorage.Api/Configuration/NinjectConfig.cs
+++ src/TodoStorage.Api/Configuration/NinjectConfig.cs
@@ -1,6 +1,6 @@
 /*
  * Todo Storage for wifeys Todo app.
- * Copyright (C) 2016  Simon Wendel
+ * Copyright (C) 2016-2017  Simon Wendel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
